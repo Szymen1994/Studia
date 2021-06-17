908cb36abec55b69ea47958bc86e4ced66ee9168
Szymen1994 <61362970+Szymen1994@users.noreply.github.com>
2021-06-17 19:27:25 +0200

    Add files via upload
    Stworzenie prostej strony z hiperłączem dołączonym do awatarów/zmianą rozmiaru czcionki/zmianą tła

---
+++ HTML-CSS-Javascript/Zadanie1/script.js
@@ -0,0 +1,31 @@
+window.onload = function(){
+document.getElementById("max").
+onclick = function() {
+  document.body.className = "plus";
+}
+
+document.getElementById("min").
+onclick = function() {
+  document.body.className = "minus";
+}
+
+document.getElementById("reset").
+onclick = function() {
+  document.body.className = "normal"
+}
+
+document.getElementById("niebieski").
+onclick = function() {
+  document.body.className = "niebieski"
+}
+
+document.getElementById("zielony").
+onclick = function() {
+  document.body.className = "zielony"
+}
+
+document.getElementById("bialy").
+onclick = function() {
+  document.body.className = "bialy"
+}
+}
